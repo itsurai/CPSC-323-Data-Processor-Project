8bc7f91a3789591bb786b220218b6ccc1b4611d2
Eduardo Guerra <127904198+itsurai@users.noreply.github.com>
2024-10-10 21:46:02 -0700

    Established a string to hold the code a user may want to clean up.

--- CPSC 323 Data Processor Project/DataProcessor.py
+++ CPSC 323 Data Processor Project/DataProcessor.py
@@ -1,3 +1,26 @@
-print("Hello world!")
+#Main Function
+def main():
+    print("Debug line 1")
 
-print("Hello again!")
+    #Anything that you want to remove extra spaces from & tokenize goes in the string 'inputCode'
+    inputCode = """
+    # This is a sample code
+    # It adds two numbers
+    
+    def add(a, b):
+        # Add two numbers
+        result = a +     b
+        return result
+    
+    # Test the function
+    print(add(5, 3))
+    """
+
+    print(inputCode)
+    print("End of main function")
+    
+
+
+
+if __name__ == "__main__":
+    main()
